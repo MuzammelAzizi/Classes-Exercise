8fdb57ff893b5c50c70e168ee2d949d93c4f4318
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-02-28 00:36:54 -0800

    status

---
+++ Classes/Cars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Cars
+    {
+        public Cars()
+        {
+        }
+
+        public Cars(string make, string model,string enginesize, int year)
+        {
+            Make = make;
+            Model = model;
+            EngineSize = enginesize;
+            Year = year;
+        }
+    
+
+        //Create a class named Car - make sure it is public
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineSize { get; set; }
+        public int Year { get; set; }
+        
+    }
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Cars();
+            myCar.Make = "Bugatti";
+            myCar.Model = "Veyron";
+            myCar.EngineSize = "8.0LW16";
+            myCar.Year = 2024;
+
+
+            var lamborghini = new Cars()
+            {
+                Make = "Lamborghini",
+                Model = "Urus",
+                EngineSize = "4.0LV8",
+                Year = 2020
+            };
+
+            var MercedesBenz = new Cars()
+            {
+                Make = "MercedesBenz",
+                Model = "C63amg",
+                EngineSize = "4.0V8",
+                Year = 2022
+            };
+
+        
+        
+            var carList = new List<Cars>() { myCar, lamborghini, MercedesBenz };
+
+            Console.WriteLine(carList);
+        
+            foreach (var car in carList) 
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.EngineSize} {car.Year}");
+            }
+        
         }
+
+
+
     }
 }
